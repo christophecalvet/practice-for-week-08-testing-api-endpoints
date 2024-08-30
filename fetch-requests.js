30f33f8d14c6f7c6b6a5286661a90faa674a9829
Christophe Calvet <onlyatest@mytest.com>
2024-08-30 15:13:56 +0200

    done

--- fetch-requests.js
+++ fetch-requests.js
@@ -8,6 +8,15 @@ Paste your code for fetch requests here once you finish each task.
   Make a request with fetch request to GET /posts and print the response
   components to the console.
 */
+// fetch("/posts")
+//   .then(readableStream => readableStream.text())
+//   .then(bodytext => console.log(bodytext))
+//   .catch(err => console.error("Error: " +  err))
+
+  fetch("/posts")
+  .then(readableStream => readableStream.json())
+  .then(bodyjson => console.log(bodyjson))
+  .catch(err => console.error("Error: " +  err))
 
 // Your code here
 
@@ -18,5 +27,22 @@ Paste your code for fetch requests here once you finish each task.
   Make a request with fetch request to POST /posts and print the response
   components to the console.
 */
+
+
+fetch('/posts', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    message: "New Post!"
+  })
+})
+.then(readableStream => readableStream.json())
+.then(bodyjson => console.log(bodyjson))
+.catch(err => console.error("Error: " +  err))
+
+
+
 
 // Your code here
